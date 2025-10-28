8cacf5a2860e2f46e293205684e9407c1c7c718f
mcshr <hruzdovmaksym@gmail.com>
2025-10-28 17:17:21 +0200

    Fix non-clickable rvExamples in card item Fix incomplete RecyclerView display on edit word screen Implement scrollable layout on home screen

--- app/src/main/java/com/mcshr/wordloom/presentation/learningScreen/CardViewHolder.kt
+++ app/src/main/java/com/mcshr/wordloom/presentation/learningScreen/CardViewHolder.kt
@@ -1,14 +1,36 @@
 package com.mcshr.wordloom.presentation.learningScreen
 
+import android.annotation.SuppressLint
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import com.mcshr.wordloom.databinding.ItemCardBinding
+
 
 class CardViewHolder(val binding: ItemCardBinding): RecyclerView.ViewHolder(binding.root){
     val controller = CardFlipController(binding)
     init{
         binding.root.setOnClickListener {
             controller.flip()
+        }
+        val gestureDetector = GestureDetector(
+            binding.root.context,
+            object : GestureDetector.SimpleOnGestureListener() {
+                override fun onSingleTapUp(e: MotionEvent): Boolean {
+                    controller.flip()
+                    return true
+                }
+
+                override fun onDown(e: MotionEvent): Boolean {
+                    return true
+                }
+            })
+        @SuppressLint("ClickableViewAccessibility")
+        binding.rvExamples.setOnTouchListener { v, event ->
+            v.performClick()
+            gestureDetector.onTouchEvent(event)
+            false
         }
     }
 
